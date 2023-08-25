df54fb72cc56dfdbe0eb055a29272b9dcbc353eb
Lenon Ricardo Mendes <lenonricardomendes@gmail.com>
2023-08-25 17:10:44 -0300

    fix: db permissions

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,6 @@ import axios, { type AxiosInstance } from 'axios'
 
 const api: AxiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
-  // withCredentials: true,
   headers: {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Credentials': 'True'
