c4788311325e5a9ec8f825a2ba68d50988cc9250
Lenon Ricardo Mendes <lenonricardomendes@gmail.com>
2023-08-25 16:50:01 -0300

    chore: fix env var

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios, { type AxiosInstance } from 'axios'
 
 const api: AxiosInstance = axios.create({
-  baseURL: process.env.API_BASE_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   // withCredentials: true,
   headers: {
     'Access-Control-Allow-Origin': '*',
